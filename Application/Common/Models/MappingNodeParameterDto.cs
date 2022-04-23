b0bd5c66f091e2cc472e194d27ef1323fc017f8a
GJ <gj_jansen@hotmail.com>
2022-04-24 01:04:28 +0200

    Stuck on modal with different data types

--- Application/Common/Models/MappingNodeParameterDto.cs
+++ Application/Common/Models/MappingNodeParameterDto.cs
@@ -7,7 +7,39 @@ public class MappingNodeParameterDto
 {
     public string Name { get; set; }
     public DataType DateType { get; set; }
-    public string Value { get; set; }
+    public IValue Value { get; set; }
     public string Description { get; set; }
     public string Placeholder { get; set; }
+
+    public void SetValue(dynamic value)
+    {
+        Value = value;
+    }
+}
+
+public interface IValue
+{
+    public void SetValue(string value);
+    public void SetValue(DateTime value);
+    public void SetValue(int value);
+}
+
+public class StringValue : IValue
+{
+    private dynamic _value = null;
+
+    public void SetValue(string value)
+    {
+        _value = value;
+    }
+
+    public void SetValue(DateTime value)
+    {
+        _value = value;
+    }
+
+    public void SetValue(int value)
+    {
+        _value = value;
+    }
 }

40d9678041edfe17b542334ae343f3d8f6102e8d
GJ <gj_jansen@hotmail.com>
2022-05-23 23:17:14 +0200

    Working on modal

---
+++ WebUI/Models/ConfirmationArgumentsModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VideoVault.WebUI.Models
+{
+    public class ConfirmationArgumentsModel
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public Func<bool>  OnConfirm { get; set; }
+    }
+}

b308734c589cf48ca04aaf230ec36c8f280b04ca
GJ <gj_jansen@hotmail.com>
2022-05-25 00:30:45 +0200

    Fixed modal confirm

--- Application/Common/Models/MappingNodeDto.cs
+++ Application/Common/Models/MappingNodeDto.cs
@@ -7,7 +7,7 @@ namespace VideoVault.Application.Common.Models
     {
         public Guid Guid { get; set; } 
         public string Name { get; set; }
-        public string? AssemblyName { get; set; }
+        public string AssemblyName { get; set; }
         public string FullName { get; set; }
         public string FriendlyName { get; set; }
         public List<MappingNodeParameterDto> Parameters { get; set; }
